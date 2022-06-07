f1cadfb2684b39704087a370d7a82a16c7be89d9
Diego Armando Perea <diego.perea.dp@gmail.com>
2022-06-07 00:34:01 -0500

    [TASK]: countdownSpaceX

---
+++ src/components/style.tsx
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/styles';
+
+export default makeStyles({
+  poster: {
+    position: 'relative',
+    top: '50',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: '150px'
+  },
+  cardContainer: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.95,
+    marginBottom: 35,
+    textAlign: 'center',
+    justifyContent: 'center',
+    padding: '55px'
+  },
+  cardContainerTimer: {
+    width: '100%',
+    height: '200%',
+    opacity: 0.90,
+    marginBottom: 35,
+    textAlign: 'center',
+    justifyContent: 'center',
+    padding: '55px'
+  }
+});
